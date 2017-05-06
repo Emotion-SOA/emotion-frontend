d79dec0a87bb5fedbfff3f3f5c96c6ff8fdd932f
wangxin <14302010064@fudan.edu.cn>
2017-05-07 02:35:18 +0800

    add scale to implement the animation effects of map moving.

--- src/pages/emotion/emotion.ts
+++ src/pages/emotion/emotion.ts
@@ -23,20 +23,25 @@ export class EmotionPage {
   loadMap() {
     let map = new BMap.Map(this.mapElement.nativeElement,{enableMapClick:false,minZoom:8,maxZoom:20});
     let geolocation = new BMap.Geolocation();
-    let gl_r;
-    let width = 200;
-    let height = 250;
+    let size = 200;
     let divCtrl;
     divCtrl = loadCtrlDiv();
     divCtrl.hide();
+    var top_right_navigation = new BMap.NavigationControl({anchor: 1, type: 1});
+    map.addControl(top_right_navigation);
 
     geolocation.getCurrentPosition(function(r){
       if(this.getStatus() == 0){
         var mk = new BMap.Marker(r.point);
         map.addOverlay(mk);
         map.panTo(r.point);
-        gl_r = r;
-        loadOverlay();
+
+        // todo get the imgsrc, abbreviation_txt, txt from server.
+        let txt = "No pain, No gain. That's the truth.";
+        loadOverlay(r.point,"imgs/img0.jpeg", txt);
+        let _txt = "No pain, No gain. That's the fake.";
+        loadOverlay(new BMap.Point(r.point.lng - 0.0005,r.point.lat - 0.0005),"imgs/img0.jpeg", _txt);
+
         map.centerAndZoom(r.point,17);
         map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
       }
@@ -45,21 +50,37 @@ export class EmotionPage {
       }
     },{enableHighAccuracy: true});
 
-    function loadOverlay() {
-      function MotionOverlay(point, imgsrc, text) {
+    function loadOverlay(point, imgsrc, text) {
+      function MotionOverlay(point) {
         this._point = point;
         this._imgsrc = imgsrc;
         this._text = text;
       }
-      function scaleByZoom(size) {
-        // return (map.getZoom()) * size * 0.1;
-        return (size * (map.getZoom()) * 0.02);
+      function scale() {
+        let center = map.getCenter();
+        let distance = map.getDistance(point, center);
+        let zoom = map.getZoom();
+        if(distance < 10){
+          distance = 10;
+        }
+        if(zoom < 10){
+          distance = 8000;
+        }
+        else if(zoom < 17){
+          distance = 2000;
+        }
+        else if(zoom < 18){
+          distance = 1000;
+        }
+        let factor = 0.8;
+        let scaledSize = (size * 0.002 * (Math.pow(zoom,3) * 0.01 * factor + 8000/distance*(1-factor)));
+        return scaledSize;
       }
+
       function setInfo(imgsrc, text){
         divCtrl._img.setAttribute("src", imgsrc);
         divCtrl._div.getElementsByTagName("span")[0].innerHTML = text;
       }
-
       MotionOverlay.prototype = new BMap.Overlay();
       MotionOverlay.prototype.initialize = function (map) {
         this._map = map;
@@ -72,8 +93,8 @@ export class EmotionPage {
           div.style.color = "white";
           div.style.padding = "1px";
           div.style.lineHeight = "20px";
-          div.style.width = scaleByZoom(width) + "px";
-          div.style.height = scaleByZoom(height) + "px";
+          div.style.width = scale() + "px";
+          div.style.height = scale() + "px";
           div.style.fontSize = "5px";
           div.style.borderRadius = "5px 5px 5px 5px";
         }
@@ -109,24 +130,28 @@ export class EmotionPage {
           }
         });
 
+        map.addEventListener("dragend", function() {
+          that._div.style.width = scale() + "px";
+          that._div.style.height = scale() + "px";
+        })
+
         map.getPanes().labelPane.appendChild(div);
         return div;
       }
+
       MotionOverlay.prototype.draw = function () {
         let map = this._map;
         let pixel = map.pointToOverlayPixel(this._point);
-        this._div.style.left = pixel.x  + "px";
-        this._div.style.top = pixel.y - 30 + "px";
+        this._div.style.left = pixel.x  - scale()/2 + "px";
+        this._div.style.top = pixel.y - scale() + "px";
       }
 
-      // todo get the imgsrc, abbreviation_txt, txt from server.
-      let txt = "No pain, No gain. That's the truth.";
-      let myCompOverlay = new MotionOverlay(gl_r.point, "imgs/img0.jpeg",  txt);
+      let myCompOverlay = new MotionOverlay(point);
       map.addOverlay(myCompOverlay);
       map.addEventListener("zoomend", function () {
         // alert("当前地图中心点：" + map.getCenter().lng + "," + map.getCenter().lat);
-        myCompOverlay._div.style.width = scaleByZoom(width) + "px";
-        myCompOverlay._div.style.height = scaleByZoom(height) + "px";
+        myCompOverlay._div.style.width = scale() + "px";
+        myCompOverlay._div.style.height = scale() + "px";
       });
     }
     function loadCtrlDiv() {
